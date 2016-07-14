9c662c3af44b66e6b095c9b36e89a11f0f2972ec
DoubleABattery <alanaarjune@gmail.com>
2016-07-14 13:33:40 -0400

    lexeme list

--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Lexeme List function
+//Passes in output file for output printing and the current LL
+void displayLexemeList(LexemeList *lists, FILE *fileout) {
+
+    int i;
+
+    //If there is a LL
+    if (lists != NULL) {
+
+        //Print the size
+        printf("Size: %ld\n", lists->size);
+
+        //FOR LOOP: From i equals 0 to the size of the list
+        for (i = 0; i < lists->size; i++) {
+
+            //Print out the list through each index i
+            printf("%d ", lists->list[i]);
+
+            //If the current value at index i happens to be an identifier
+            //Increase i
+            if (lists->list[i] == identsym) {
+                i++;
+
+                //If i is less than the size of the list, print
+                if (i < lists->size)
+                    printf("%s ", lists->symbols[lists->list[i]]);
+            }
+        }
+    }
+}
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
 }
