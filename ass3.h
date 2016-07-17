d581ce8a7d10e9c7b34ec88a5218ae0204d10596
DoubleABattery <alanaarjune@gmail.com>
2016-07-17 11:45:20 -0400

    Update + Some stuff needed for main

--- ass3.h
+++ ass3.h
@@ -1,1 +1,25 @@
 //Assignment 3 header
+
+typedef struct {
+    int kind;   //const, int proc
+    char *name;   //obvs the name/identifier
+    int value;    //ASCII value
+    int level;    //L level
+    int address;    //M address
+    } symbols;
+
+typedef struct {
+    int op, l, m;
+    } instruct;
+    
+    
+typedef enum {
+    lit = 1; opr, lod, sto, cal, inc, jmp, jpc, sioIN, sioOUT
+    }vmInstruct;
+    
+LexemeList *LL;
+int pointLL;
+symbols *symTable;
+
+//Prototypes
+void getToken();
